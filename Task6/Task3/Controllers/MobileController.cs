32c4c3bb5345912c63f2d73abd1e24705cc2d55d
LuxuryAndrew <LuxuryAndrew@gmail.com>
2016-07-19 00:44:53 +0300

    first commit

---
+++ Task6/Task3/Controllers/MobileController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Security;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Task3.Models;
+
+namespace Task3.Controllers
+{
+    public class MobileController : ApiController
+    {
+        private FabricContext db = new FabricContext();
+
+        // GET: api/Mobile
+        [Authorize]
+        public IEnumerable<string> Get()
+        {
+            string userId = User.Identity.GetUserId();
+            IEnumerable<string> result = db.Fabrics.Where(c => c.UserId == userId).ToList().Select(fabric => Url.Content($"~/Content/fabrics/{fabric.Id}/thumbnail.png"));
+            return result;
+        }
+    }
+}
---
+++ Task6/Task3/Models/keyAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Task3.Models
+{
+    internal class keyAttribute : Attribute
+    {
+    }
+}
