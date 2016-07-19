53e7b3802bef4eef73fd5644a08b1c83e496cd66
LuxuryAndrew <LuxuryAndrew@gmail.com>
2016-07-19 08:54:38 +0300

    changes

---
+++ Task6/Task3/App_Start/Startup.Auth.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Google;
+using Owin;
+using Task3.Models;
+using Owin.Security.Providers.GitHub;
+using Owin.Security.Providers.Instagram;
+
+namespace Task3
+{
+    public partial class Startup
+    {
+        // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            // Configure the db context, user manager and signin manager to use a single instance per request
+            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
+
+            // Enable the application to use a cookie to store information for the signed in user
+            // and to use a cookie to temporarily store information about a user logging in with a third party login provider
+            // Configure the sign in cookie
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+                Provider = new CookieAuthenticationProvider
+                {
+                    // Enables the application to validate the security stamp when the user logs in.
+                    // This is a security feature which is used when you change a password or add an external login to your account.  
+                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
+                        validateInterval: TimeSpan.FromMinutes(30),
+                        regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
+                }
+            });            
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            // Enables the application to temporarily store user information when they are verifying the second factor in the two-factor authentication process.
+            app.UseTwoFactorSignInCookie(DefaultAuthenticationTypes.TwoFactorCookie, TimeSpan.FromMinutes(5));
+
+            // Enables the application to remember the second login verification factor such as phone or email.
+            // Once you check this option, your second step of verification during the login process will be remembered on the device where you logged in from.
+            // This is similar to the RememberMe option when you log in.
+            app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
+
+            // Uncomment the following lines to enable logging in with third party login providers
+            //app.UseMicrosoftAccountAuthentication(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            app.UseTwitterAuthentication(
+               consumerKey: "RKNF9u1oSmjCIb4tdVO8aZE93",
+               consumerSecret: "2A6AJoU1Bn1FTpQAPWq5m9xkwKYnVo1abvqurEvGm0oiAkwDHY");
+
+            app.UseFacebookAuthentication(
+               appId: "404716249729610",
+               appSecret: "e08cc9022de00dcc13b219015128fe38");
+
+            app.UseVkontakteAuthentication("5033403", "aCDgMQqkxkcTC0iT2dT3", "scope=email");
+
+            app.UseGitHubAuthentication("291b136230cda46b706f", "64936068f2663318e18f45a0d96744dd1920fd54");
+
+            app.UseInstagramInAuthentication("42df8d7d3bc845eeb3ad579ca5d35822", "c4330872c19d47aaaced6a51d2f07c22");
+        }
+    }
+}
